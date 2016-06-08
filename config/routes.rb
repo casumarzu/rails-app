b80e094cee92e4f138ba075a5ab2e618a49679a5
andrey <casumarzu@yandex.ru>
2016-06-08 15:51:56 +0300

    Add localization

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :products
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  devise_for :users
-  get 'persons/profile'
-
-  root 'photos#index'
-  get 'persons/profile', as: 'user_root'
-  resources :photos
+  scope "(:locale)", locale: /ru|en/ do
+    resources :products
+    devise_for :admin_users, ActiveAdmin::Devise.config
+    ActiveAdmin.routes(self)
+    devise_for :users
+    get 'persons/profile'
+    root to: redirect("/photos")
+    get 'persons/profile', as: 'user_root'
+    resources :photos
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
