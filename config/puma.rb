e01caa797788f115a7cbab0f56521c1308ab7502
andrey <casumarzu@yandex.ru>
2016-08-30 13:18:47 +0300

    Change puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,20 +1,29 @@
+#
+# threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+# threads threads_count, threads_count
+#
+# port        ENV.fetch("PORT") { 3000 }
+#
+# environment ENV.fetch("RAILS_ENV") { "development" }
+#
+# app_dir = File.expand_path("../..", __FILE__)
+# puts "App dir: #{app_dir}"
+# shared_dir = "#{app_dir}/shared/tmp"
+# bind "unix://#{shared_dir}/sockets/puma.sock"
+#
+# stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+#
+# pidfile "#{shared_dir}/pids/puma.pid"
+# state_path "#{shared_dir}/pids/puma.state"
+# activate_control_app
+#
+# plugin :tmp_restart
 
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
+port ENV.fetch("PORT") { 3000 }
+environment "development"
 
-port        ENV.fetch("PORT") { 3000 }
+bind  "unix:///var/www/fatsquash.ru/rails-app/shared/tmp/sockets/puma.sock"
+pidfile "/var/www/fatsquash.ru/rails-app/shared/tmp/puma.pid"
+state_path "/var/www/fatsquash.ru/rails-app/shared/tmp/puma.state"
 
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-app_dir = File.expand_path("../..", __FILE__)
-puts "App dir: #{app_dir}"
-shared_dir = "#{app_dir}/shared/tmp"
-bind "unix://#{shared_dir}/sockets/puma.sock"
-
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
-
-plugin :tmp_restart
